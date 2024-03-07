8c52b7387e84856893ea1bd3039dc61703e4db8a
Chandu_Kommineni <chandukommineni63@gmail.com>
2024-03-07 14:08:16 +0530

    Queue is Modified

--- LinearDataStructures/Queue.py
+++ LinearDataStructures/Queue.py
@@ -12,19 +12,44 @@ class Queue:
             self.front+=1 
             self.rear+=1 
             self.Queue.append(val)
+
         elif self.size==len(self.Queue[self.front:self.rear+1]):
             print('Queue is full') 
         else:
             self.Queue.append(val)
             self.rear+=1 
-    
+    def dequeue(self):
+        if self.rear==-1:
+            print("Queue is empty")
+        elif self.front==self.rear:
+            print('element popped is ',self.Queue[self.rear])
+            self.front=-1 
+            self.rear=-1 
+        else:
+            print('element popped is ',self.Queue[self.front])
+            self.front+=1       
+            
     def traverse(self):
+        if self.front==-1 and self.rear==-1:
+            print('queue is empty')
+            return
         for i in range(self.front,self.rear+1):
             print(self.Queue[i],end=' ')
-Q=Queue(2)
-Q.enqueue(10)
-Q.enqueue(30)
-Q.enqueue(20)
-Q.traverse()
+        print()
+
+size=int(input("Enter the size of Queue"))
+Q=Queue(size)
+while True :
+    choice=int(input("Select the operation\n1.enqueue\n2.dequeue\n3.traverse\n4.exit\n"))
+    if choice==1:
+        Q.enqueue(int(input("enter value to insert\n")))
+    elif choice==2:
+        Q.dequeue()
+    elif choice==3:
+        Q.traverse()
+    else:
+        break
+
+
 
 
